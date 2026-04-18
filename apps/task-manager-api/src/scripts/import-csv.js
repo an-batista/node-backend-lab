47fdb054b60054c10f61d37d2b759f37ac1e57c5
Angelo Rafael <113860124+RafaSicron@users.noreply.github.com>
2026-04-18 12:38:30 -0300

    feat(task): implement csv task importer using POST request

--- apps/task-manager-api/src/scripts/import-csv.js
+++ apps/task-manager-api/src/scripts/import-csv.js
@@ -6,8 +6,17 @@ parse({
     skip_empty_lines: true
 })
 
-const stream = createReadStream("./src/data/createTask.csv").pipe(parse({ columns: true }))
+const csvStream = createReadStream("./src/data/createTask.csv").pipe(parse({ columns: true }))
 
-for await (const record of stream) {
-    console.log(record)
+for await (const body of csvStream) {
+    const fetchResponse = await fetch("http://localhost:3333/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    })
+    try {
+        console.log(fetchResponse.status)
+    } catch (error) {
+        console.log(error)
+    }
 }
