ad3b7d3ed9410bb95848650a59e57bfc58a35c6b
an-batista <113860124+an-batista@users.noreply.github.com>
2026-04-20 22:07:38 -0300

    chore(diet): initial knex setup

---
+++ apps/diet-manager-api/src/database.ts
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import knex, { Knex } from "knex"
+import { env } from "./env"
+
+
+export const config: Knex.Config = {
+    client: "sqlite3",
+    connection: {
+        filename: env.DATABASE_URL
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: "ts",
+        directory:"./db/migrations"
+    }
+}
+
+export const db = knex(config)
