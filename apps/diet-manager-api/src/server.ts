34c4eba8af46716e9020541173b7f0f9f08c73f5
an-batista <113860124+an-batista@users.noreply.github.com>
2026-04-29 23:03:34 -0300

    feat(diet): add mealsRoute file

--- apps/diet-manager-api/src/server.ts
+++ apps/diet-manager-api/src/server.ts
@@ -1,7 +1,11 @@
 import fastify from "fastify"
-
+import { mealsRoutes } from "./routes/mealsRoutes"
 
 const app = fastify()
+
+app.register(mealsRoutes, {
+    prefix:"meals"
+})
 
 app.listen({
     port: 3333
