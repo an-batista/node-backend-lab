bf82e2d72be93be26f06a5612dedb6ffb9ab8fed
an-batista <113860124+an-batista@users.noreply.github.com>
2026-05-07 21:55:21 -0300

    feat(diet): create POST method to users.routes

---
+++ apps/diet-manager-api/src/routes/users.routes.ts
@@ -0,0 +1,26 @@
+import type { FastifyInstance } from "fastify";
+import z, { email, uuid } from "zod";
+import { id } from "zod/locales";
+import { db } from "../database";
+import { randomUUID } from "node:crypto";
+
+
+
+export async function usersRoutes(app: FastifyInstance) {
+    app.post("/", async (request, reply) => {
+        const requestBodySchema = z.object({
+            name: z.string(),
+            email: z.string(),
+            password: z.number()
+        })
+
+        const { name, email, password } = requestBodySchema.parse(request.body)
+        await db("users").insert({
+            id: randomUUID(),
+            name,
+            email,
+            password
+        })
+        return reply.status(201).send()
+    })
+}
